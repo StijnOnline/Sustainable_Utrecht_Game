b6f7b2925decda029cc93557264cbb6b02c46f21
StijnOnline <stijnvandeijzen@gmail.com>
2020-02-05 11:34:48 +0100

    Recycling Minigame basic functionality

--- Sustainable_Utrecht_Game/Assets/Scripts/TouchSystem.cs
+++ Sustainable_Utrecht_Game/Assets/Scripts/TouchSystem.cs
@@ -2,14 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TouchSystem : MonoBehaviour
-{
+public class TouchSystem : MonoBehaviour {
     //Singleton Access
     private static TouchSystem _instance;
     public static TouchSystem Instance {
         get {
-            if (_instance == null)
-            {
+            if(_instance == null) {
                 GameObject g = new GameObject("TouchSystem");
                 _instance = g.AddComponent<TouchSystem>();
             }
@@ -20,46 +18,82 @@ public class TouchSystem : MonoBehaviour
 
     [Header("Settings")]
     [SerializeField] private LayerMask tappableMask = 0;
+    [SerializeField] private LayerMask draggableMask = 0;
+    [SerializeField] private bool dragging = false;
+    [SerializeField] private bool mouse = false;
 
-    private void Awake()
-    {
-        if (_instance != null && _instance != this)
-        {
+    private IDraggable draggingObject;
+
+    private void Awake() {
+        if(_instance != null && _instance != this) {
             Destroy(this.gameObject);
-        }
-        else
-        {
+        } else {
             _instance = this;
         }
     }
 
-    void Start()
-    {
-        
+    void Start() {
+
     }
 
 
 
-    void Update()
-    {
-        if (Input.touchCount > 0)
-        {
-            Touch touch = Input.GetTouch(0);
+    void Update() {
+        if((!mouse && Input.touchCount > 0) || (mouse && Input.GetMouseButton(0))) {
+            Vector2 pos;
+            Touch touch = new Touch();
+            if(!mouse) {
+                touch = Input.GetTouch(0);
+                pos = touch.position;
+            } else {
+                pos = Input.mousePosition;
+            }
+            Vector3 touchedPos = Camera.main.ScreenToWorldPoint(new Vector3(pos.x, pos.y, 10));
 
 
-            if (touch.phase == TouchPhase.Began)
-            {
-            Vector3 touchedPos = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 10));
-            Ray touchRay = new Ray(Vector3.forward, touchedPos);
-            RaycastHit2D hit = Physics2D.Raycast(touchedPos, Vector2.zero, 10, tappableMask);
+            if((!mouse && touch.phase == TouchPhase.Began) || (mouse && Input.GetMouseButtonDown(0))) {
+                Ray touchRay = new Ray(Vector3.forward, touchedPos);
+                RaycastHit2D hit = Physics2D.Raycast(touchedPos, Vector2.zero, 10, tappableMask);
 
-                if (hit)
-                {
+                if(hit) {
                     IClickable clickable = hit.collider.GetComponent<IClickable>();
-                    clickable.Click();
+                    if(clickable != null)
+                        clickable.Click();
+
+                }
+
+                if(dragging) {
+                    hit = Physics2D.Raycast(touchedPos, Vector2.zero, 10, draggableMask);
+                    if(hit) {
+                        IDraggable draggable = hit.collider.GetComponent<IDraggable>();
+                        if(draggable != null) {
+                            if(draggingObject != draggable)
+                                draggingObject = draggable;
+                            draggable.Pick();
+                        }
+                    }
                 }
 
             }
+
+            if((!mouse && (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)) || (mouse && Input.GetMouseButtonUp(0))) {
+                if(dragging) {
+
+                    if(draggingObject != null) {
+                        draggingObject.Drop();
+                    }
+                    draggingObject = null;
+                }
+            }
+
+            if(dragging && draggingObject != null) {
+                draggingObject.UpdatePos(touchedPos);
+            }
+
         }
+
+
+
+
     }
 }
