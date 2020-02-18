24a258b7a476f385d9b477b044d304d9d8eb5be2
StijnOnline <stijnvandeijzen@hotmail.nl>
2020-02-18 14:11:25 +0100

    CameraPan

---
+++ Sustainable_Utrecht_Game/Assets/Scripts/CameraPan.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+class CameraPan : MonoBehaviour {
+
+    private TouchSystem touchSystem;
+    private Camera cam;
+    Vector3 targetPos;
+
+    private void Start() {
+        touchSystem = TouchSystem.Instance;
+        cam = GetComponent<Camera>();
+        touchSystem.startTouch += StartPan;
+    }
+
+    private void Update() {
+        if(touchSystem.touching) {
+            transform.Translate(targetPos - cam.ScreenToWorldPoint(touchSystem.lastTouchedScreenPos));
+        }
+    }
+
+    void StartPan(Vector2 pos) {
+        targetPos = cam.ScreenToWorldPoint(pos);
+    }
+}
--- Sustainable_Utrecht_Game/Assets/Scripts/TouchSystem.cs
+++ Sustainable_Utrecht_Game/Assets/Scripts/TouchSystem.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TouchSystem : MonoBehaviour {
+
+    //TODO constructor
+
+
     //Singleton Access
     private static TouchSystem _instance;
     public static TouchSystem Instance {
@@ -16,11 +20,15 @@ public class TouchSystem : MonoBehaviour {
         }
     }
 
+    public System.Action<Vector2> startTouch;
+
     [Header("Settings")]
     [SerializeField] private LayerMask tappableMask = 0;
     [SerializeField] private LayerMask draggableMask = 0;
     [SerializeField] private bool dragging = false;
-    [SerializeField] private bool mouse = false;
+
+    public bool touching { get; private set; } = false;
+    public Vector2 lastTouchedScreenPos { get; private set; }
 
     private IDraggable draggingObject;
 
@@ -39,63 +47,70 @@ public class TouchSystem : MonoBehaviour {
 
 
     void Update() {
-        if((!mouse && Input.touchCount > 0) || (mouse)) {
-            Vector2 pos;
-            Touch touch = new Touch();
-            if(!mouse) {
-                touch = Input.GetTouch(0);
-                pos = touch.position;
-            } else {
-                pos = Input.mousePosition;
-            }
-            Vector3 touchedPos = Camera.main.ScreenToWorldPoint(new Vector3(pos.x, pos.y, 100000));
+        if(Input.GetMouseButton(0))
+            touching = true;
 
 
-            if((!mouse && touch.phase == TouchPhase.Began) || (mouse && Input.GetMouseButtonDown(0))) {
-                Ray touchRay = new Ray(Vector3.forward, touchedPos);
-                RaycastHit2D hit = Physics2D.Raycast(touchedPos, Vector2.zero, Mathf.Infinity, tappableMask);
-
-                if(hit) {
-                    Transform checking = hit.transform;
-                    IClickable clickable = checking.GetComponent<IClickable>(); ;
-                    while(clickable == null && checking.parent != null)
-                    {
-                        checking = checking.parent;
-                        clickable = checking.GetComponent<IClickable>();
-
-                    } 
+        Vector2 pos;
+        Touch touch = new Touch();
+        if(Input.touchCount > 0) {
+            touch = Input.GetTouch(0);
+            pos = touch.position;
+            touching = true;
+        }
+        pos = Input.mousePosition;
+        lastTouchedScreenPos = pos;
+        Vector3 touchedPos = Camera.main.ScreenToWorldPoint(new Vector3(pos.x, pos.y, 100000));
 
 
-                    if(clickable != null)
-                        clickable.Click();
+        if((Input.touchCount > 0 && touch.phase == TouchPhase.Began) || Input.GetMouseButtonDown(0)) {
+            startTouch.Invoke(pos);
+
+            Ray touchRay = new Ray(Vector3.forward, touchedPos);
+            RaycastHit2D hit = Physics2D.Raycast(touchedPos, Vector2.zero, Mathf.Infinity, tappableMask);
+
+            if(hit) {
+                Transform checking = hit.transform;
+                IClickable clickable = checking.GetComponent<IClickable>(); ;
+                while(clickable == null && checking.parent != null) {
+                    checking = checking.parent;
+                    clickable = checking.GetComponent<IClickable>();
 
                 }
 
-                if(dragging && draggingObject == null) {
-                    hit = Physics2D.Raycast(touchedPos, Vector2.zero, 10, draggableMask);
-                    if(hit) {
-                        IDraggable draggable = hit.collider.GetComponent<IDraggable>();
-                        if(draggable != null) {
-                            draggingObject = draggable;
-                            draggingObject.Pick();
-                        }
+
+                if(clickable != null)
+                    clickable.Click();
+
+            }
+
+            if(dragging && draggingObject == null) {
+                hit = Physics2D.Raycast(touchedPos, Vector2.zero, 10, draggableMask);
+                if(hit) {
+                    IDraggable draggable = hit.collider.GetComponent<IDraggable>();
+                    if(draggable != null) {
+                        draggingObject = draggable;
+                        draggingObject.Pick();
                     }
                 }
-
-            }
-
-            if((!mouse && (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)) || (mouse && Input.GetMouseButtonUp(0))) {
-                if(dragging && draggingObject != null) {
-                    draggingObject.Drop();
-                    draggingObject = null;
-                }
-            }
-
-            if(dragging && draggingObject != null) {
-                draggingObject.UpdatePos(touchedPos);
             }
 
         }
+
+
+        if((Input.touchCount > 0 && touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled) || Input.GetMouseButtonUp(0)) {
+            touching = false;
+            if(dragging && draggingObject != null) {
+                draggingObject.Drop();
+                draggingObject = null;
+            }
+        }
+
+        if(dragging && draggingObject != null) {
+            draggingObject.UpdatePos(touchedPos);
+        }
+
+
 
 
 
