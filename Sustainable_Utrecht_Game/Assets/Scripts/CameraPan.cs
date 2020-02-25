a111d7e33aa86c05730cff9f6937c1ea23368285
StijnOnline <stijnvandeijzen@hotmail.nl>
2020-02-25 12:11:07 +0100

    Camera pan limits

--- Sustainable_Utrecht_Game/Assets/Scripts/CameraPan.cs
+++ Sustainable_Utrecht_Game/Assets/Scripts/CameraPan.cs
@@ -5,7 +5,10 @@ class CameraPan : MonoBehaviour {
     private TouchSystem touchSystem;
     private Camera cam;
     Vector3 targetPos;
-
+    [SerializeField] Vector2 XMinMax;
+    [SerializeField] Vector2 YMinMax;
+    float[] minmax = new float[2];
+    
     private void Start() {
         touchSystem = TouchSystem.Instance;
         cam = GetComponent<Camera>();
@@ -14,7 +17,10 @@ class CameraPan : MonoBehaviour {
 
     private void Update() {
         if(touchSystem.touching) {
-            transform.Translate(targetPos - cam.ScreenToWorldPoint(touchSystem.lastTouchedScreenPos));
+            Vector3 camPos = transform.position + targetPos - cam.ScreenToWorldPoint(touchSystem.lastTouchedScreenPos);
+            camPos.x = Mathf.Clamp(camPos.x, XMinMax.x, XMinMax.y);
+            camPos.y = Mathf.Clamp(camPos.y, YMinMax.x, YMinMax.y);
+            transform.position = camPos;
         }
     }
 
