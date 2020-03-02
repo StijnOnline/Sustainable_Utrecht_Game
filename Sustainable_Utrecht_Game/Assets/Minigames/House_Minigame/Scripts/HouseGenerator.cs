ddb2fb5767a56e26b948cfe842f0319c64c0d4e1
StijnOnline <stijnvandeijzen@hotmail.nl>
2020-03-02 21:36:30 +0100

    Fish and House Minigames start

--- Sustainable_Utrecht_Game/Assets/Minigames/House_Minigame/Scripts/HouseGenerator.cs
+++ Sustainable_Utrecht_Game/Assets/Minigames/House_Minigame/Scripts/HouseGenerator.cs
@@ -6,12 +6,15 @@ public class HouseGenerator : MonoBehaviour {
 
 
 
+    [SerializeField] private Transform root;
+
+
     [SerializeField] private int floorCount = 3;
     [SerializeField] private float bigRoomChance = 0.5f;
 
 
     [SerializeField] private float smallRoomWidth = 2f;
-    [SerializeField] private float bigRoomWidth = 2f;
+    [SerializeField] private float mediumRoomWidth = 2f;
     [SerializeField] private float roomHeigth = 2f;
     [SerializeField] private GameObject[] smallRoomPrefabs;
     [SerializeField] private GameObject[] mediumRoomPrefabs;
@@ -26,21 +29,20 @@ public class HouseGenerator : MonoBehaviour {
     void GenerateHouse() {
 
 
-        
         float yPos = 0;
         for(int i = 0; i < floorCount; i++) {
             bool bigRoom = Random.value > bigRoomChance;
             if(bigRoom) {
-                Instantiate(bigRoomPrefabs[Random.Range(0, bigRoomPrefabs.Length)], transform.position+ Vector3.up * yPos, Quaternion.identity);
+                Instantiate(bigRoomPrefabs[Random.Range(0, bigRoomPrefabs.Length)], root.position+ Vector3.up * yPos, Quaternion.identity, root);
             } else {
                 int r = Random.Range(0, 2) * 2 - 1;
-                Instantiate(smallRoomPrefabs[Random.Range(0, smallRoomPrefabs.Length)], transform.position + Vector3.right * (-r * 0.5f * (smallRoomWidth + bigRoomWidth) + r * 0.5f * smallRoomWidth) + Vector3.up * yPos, Quaternion.identity);
-                Instantiate(mediumRoomPrefabs[Random.Range(0, mediumRoomPrefabs.Length)], transform.position + Vector3.right * (r * 0.5f * (smallRoomWidth + bigRoomWidth) + -r * 0.5f * bigRoomWidth) + Vector3.up * yPos, Quaternion.identity);
+                Instantiate(smallRoomPrefabs[Random.Range(0, smallRoomPrefabs.Length)], root.position + Vector3.right * (-r * 0.5f * (smallRoomWidth + mediumRoomWidth) + r * 0.5f * smallRoomWidth) + Vector3.up * yPos, Quaternion.identity, root);
+                Instantiate(mediumRoomPrefabs[Random.Range(0, mediumRoomPrefabs.Length)], root.position + Vector3.right * (r * 0.5f * (smallRoomWidth + mediumRoomWidth) + -r * 0.5f * mediumRoomWidth) + Vector3.up * yPos, Quaternion.identity, root);
             }
             yPos += roomHeigth;
         }
         //spawn Roof
-        Instantiate(roofPrefabs[Random.Range(0, roofPrefabs.Length)], transform.position + Vector3.up * yPos, Quaternion.identity);
+        Instantiate(roofPrefabs[Random.Range(0, roofPrefabs.Length)], root.position + Vector3.up * yPos, Quaternion.identity, root);
 
 
 
