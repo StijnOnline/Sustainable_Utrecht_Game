132c70ac56e4be299d3ed2b0a223f5f6d5c55324
StijnOnline <stijnvandeijzen@hotmail.nl>
2020-03-20 15:26:43 +0100

    Added Path Following AI

---
+++ Sustainable_Utrecht_Game/Assets/3d_City/Scripts/PathFollowAI.cs
@@ -0,0 +1,16 @@
+﻿using StijnUtility;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFollowAI : MonoBehaviour
+{
+    [SerializeField] private Path path;
+    [SerializeField] private float movespeed;
+    float d = 0;
+    
+    private void FixedUpdate() {
+        d += movespeed;
+        transform.position = path.Evaluate(d);
+    }
+}
