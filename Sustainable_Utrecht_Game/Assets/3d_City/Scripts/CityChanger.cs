9b52a9eddb4dc5bd68adda79d76258b348d56624
StijnOnline <stijnvandeijzen@hotmail.nl>
2020-03-23 15:06:43 +0100

    Added CityChanger

---
+++ Sustainable_Utrecht_Game/Assets/3d_City/Scripts/CityChanger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CityChanger : MonoBehaviour
+{
+    [SerializeField] private List<CityVariable> cityVariables;
+
+    [System.Serializable]
+    public struct CityVariable {
+        public string _name;
+        public SDGs _SDG;
+
+        public GameObject _object;
+        public Vector2 activeRange;
+
+    }
+
+    private void OnEnable() {
+        SaveData save = DataSaver.LoadData();
+        foreach(CityVariable cityVariable in cityVariables) {
+            bool inrange = (save.SDGPoints[(int)cityVariable._SDG] > cityVariable.activeRange.x && save.SDGPoints[(int)cityVariable._SDG] < cityVariable.activeRange.y);
+            if(cityVariable._object != null) {
+                cityVariable._object.SetActive(inrange);
+            }
+        }
+    }
+}
---
+++ Sustainable_Utrecht_Game/Assets/3d_City/Scripts/Editor/CityChangerEditor.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditorInternal;
+
+[CustomEditor(typeof(CityChanger))]
+public class CityChangerEditor : Editor {
+
+    SerializedProperty cityVariables;
+    private ReorderableList reorderableList;
+    void OnEnable() {
+        cityVariables = serializedObject.FindProperty("cityVariables");
+        reorderableList = new ReorderableList(serializedObject, cityVariables, true, true, true, true);
+        reorderableList.drawElementCallback = DrawElementCallback;
+        reorderableList.elementHeightCallback = ElementHeightCallback;
+        reorderableList.drawHeaderCallback = DrawHeaderCallback;
+    }
+
+    private void DrawHeaderCallback(Rect rect) {
+        EditorGUI.LabelField(rect, "City Variable Objects");
+    }
+
+    private void DrawElementCallback(Rect rect, int index, bool isactive, bool isfocused) {
+        //Get the element we want to draw from the list.
+        SerializedProperty element = reorderableList.serializedProperty.GetArrayElementAtIndex(index);
+        rect.y += 2;
+
+        //We get the name property of our element so we can display this in our list.
+        SerializedProperty elementName = element.FindPropertyRelative("_name");
+        string elementTitle = string.IsNullOrEmpty(elementName.stringValue)
+            ? "New"
+            : elementName.stringValue;
+
+        
+
+        //Draw the list item as a property field, just like Unity does internally.
+        EditorGUI.PropertyField(position:
+            new Rect(rect.x += 10, rect.y, Screen.width * .8f, height: EditorGUIUtility.singleLineHeight), property:
+            element, label: new GUIContent(elementTitle), includeChildren: true);
+    }
+
+    private float ElementHeightCallback(int index) {
+        //Gets the height of the element. This also accounts for properties that can be expanded, like structs.
+        float propertyHeight = EditorGUI.GetPropertyHeight(reorderableList.serializedProperty.GetArrayElementAtIndex(index), true);
+
+        float spacing = EditorGUIUtility.singleLineHeight / 2;
+
+        return propertyHeight + spacing;
+    }
+
+    public override void OnInspectorGUI() {
+        serializedObject.Update();
+        reorderableList.DoLayoutList();
+        serializedObject.ApplyModifiedProperties();
+    }
+}
+
