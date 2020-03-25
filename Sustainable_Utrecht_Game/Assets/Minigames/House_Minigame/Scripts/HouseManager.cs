4cee05aec4f296a6b471770583fa53c849b03eac
StijnOnline <stijnvandeijzen@hotmail.nl>
2020-03-25 16:09:38 +0100

    House Intro Animation Coded

--- Sustainable_Utrecht_Game/Assets/Minigames/House_Minigame/Scripts/HouseManager.cs
+++ Sustainable_Utrecht_Game/Assets/Minigames/House_Minigame/Scripts/HouseManager.cs
@@ -7,34 +7,86 @@ class HouseManager : MonoBehaviour {
     public static HouseManager self;
     public HouseGenerator generator;
     [SerializeField] private Transform barMask;
+    [SerializeField] private Transform roof;
     private float barMaskheight;
     private HouseItem[] houseItems;
 
     private float totalEnergy = 0;
     private float energy = 0;
-    private float defaultEnergy = 0;
+    [SerializeField] private float defaultEnergy = 0;
+
+    [SerializeField] private float fallIntroTime = 1;
+    [SerializeField] private float fallIntroDist = 60;
 
     private void Start() {
-        barMaskheight = barMask.GetComponent<Renderer>().bounds.extents.y;
+        barMaskheight = barMask.localScale.y;
         self = this;
 
         generator.GenerateHouse();
         houseItems = FindObjectsOfType<HouseItem>();
 
+        //startState
         foreach(HouseItem item in houseItems) {
-            item.turnedOn = false;
+            if(item.turnedOn) item.ChangeState();
             totalEnergy += item.energy;
         }
         totalEnergy += defaultEnergy;
+        energy = defaultEnergy;
+        UpdateBar();
+
+        //Rooms start high
+        foreach(Transform room in generator.root) {
+            if(room != generator.root) {
+                room.position += Vector3.up * fallIntroDist;
+            }
+        }
+        roof.position += Vector3.up * fallIntroDist;
 
         StartCoroutine(Intro());
     }
 
     public IEnumerator Intro() {
-        foreach(HouseItem item in houseItems) {
-            item.ChangeState();
+
+        //Drop Rooms
+        yield return new WaitForSeconds(1f);
+        for(int f = 0; f < generator.root.childCount; f++) {
+            Transform floor = generator.root.GetChild(f);
+            for(int i = 0; i < fallIntroTime * 60; i++) {
+                floor.position += Vector3.down * fallIntroDist / fallIntroTime / 60;
+                yield return 0;
+            }
             yield return new WaitForSeconds(0.2f);
+            for(int i = 0; i < 30; i++) {
+                Camera.main.transform.position += Vector3.up * generator.roomHeigth / 30;
+                yield return 0;
+            }
+
         }
+        for(int i = 0; i < fallIntroTime * 60; i++) {
+            roof.position += Vector3.down * fallIntroDist / fallIntroTime / 60;
+            yield return 0;
+        }
+
+        //Turn All On
+        yield return new WaitForSeconds(1f);
+
+        for(int f = generator.root.childCount - 1; f >= 0; f--) {
+            Transform floor = generator.root.GetChild(f);
+
+            for(int i = 0; i < 30; i++) {
+                Camera.main.transform.position += Vector3.down * generator.roomHeigth / 30;
+                yield return 0;
+            }
+            yield return new WaitForSeconds(0.2f);
+            foreach(HouseItem item in floor.GetComponentsInChildren<HouseItem>()) {
+                item.ChangeState();
+                yield return new WaitForSeconds(0.2f);
+            }
+
+        }
+
+
+
     }
 
     public void ChangeEnergy(float amount) {
@@ -43,7 +95,7 @@ class HouseManager : MonoBehaviour {
     }
 
     public void UpdateBar() {
-        barMask.localPosition = (Vector3.up * ((energy / totalEnergy) * 2 * barMaskheight - 2 * barMaskheight));
+        barMask.localPosition = (Vector3.up * ((energy / totalEnergy) * barMaskheight - barMaskheight));
     }
 
 }
